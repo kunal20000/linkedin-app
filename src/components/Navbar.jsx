ffa64be490ba1b3de65ec3979494589d49c46bf5
kunal <kunalsapate2000@gmail.com>
2023-09-05 01:48:25 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,33 +11,30 @@ export const Navbar = () => {
           viewBox="0 0 24 24"
           data-supported-dps="24x24"
           fill="currentColor"
-          className="linkdin_image"
-          width="34"
-          height="34"
-          focusable="false"
+          className="mercado-match"
+          width="40"
+          height="40"
+          focusable="true"
         >
           <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
         </svg>
         <input type="search" id="search" placeholder="Search" />
         <i className="fa fa-search"></i>
-       </section>
+      </section>
 
       <section className="navbar-container2">
         <div className="navbar-container2-home">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="19"
-            height="19"
             viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-home"
+            data-supported-dps="24x24"
+            fill="currentColor"
+            className="mercado-match1"
+            width="24"
+            height="24"
+            focusable="false"
           >
-            <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
           </svg>
           <NavLink className="homeNav" to="/">
             Home
@@ -46,21 +43,15 @@ export const Navbar = () => {
         <div className="navbar-container2-mynetwork">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="19"
-            height="19"
             viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-network"
+            data-supported-dps="24x24"
+            fill="currentColor"
+            className="mercado-match2"
+            width="24"
+            height="24"
+            focusable="false"
           >
-            <rect x="16" y="16" width="6" height="6" rx="1"></rect>
-            <rect x="2" y="16" width="6" height="6" rx="1"></rect>
-            <rect x="9" y="2" width="6" height="6" rx="1"></rect>
-            <path d="M5 16v-3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3"></path>
-            <path d="M12 12V8"></path>
+            <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
           </svg>
           <NavLink className="mynetworknav" to="/mynetwork">
             My Network
@@ -69,21 +60,15 @@ export const Navbar = () => {
         <div className="navbar-container2-jobs">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="19"
-            height="19"
             viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-baggage-claim"
+            data-supported-dps="24x24"
+            fill="currentColor"
+            className="mercado-match3"
+            width="24"
+            height="24"
+            focusable="false"
           >
-            <path d="M22 18H6a2 2 0 0 1-2-2V7a2 2 0 0 0-2-2"></path>
-            <path d="M17 14V4a2 2 0 0 0-2-2h-1a2 2 0 0 0-2 2v10"></path>
-            <rect width="13" height="8" x="8" y="6" rx="1"></rect>
-            <circle cx="18" cy="20" r="2"></circle>
-            <circle cx="9" cy="20" r="2"></circle>
+            <path d="M22.84 10.22L21 6h-3.95V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2l2.22 5.18A3 3 0 007 13h14a2 2 0 001.84-2.78zM15.05 6h-6V5a1 1 0 011-1h4a1 1 0 011 1zM7 14h15v3a3 3 0 01-3 3H5a3 3 0 01-3-3V8.54l1.3 3A4 4 0 007 14z"></path>
           </svg>
           <NavLink className="jobnav" to="/jobs">
             Jobs
@@ -92,17 +77,15 @@ export const Navbar = () => {
         <div className="navbar-container2-messaging">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="19"
-            height="19"
             viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-message-circle"
+            data-supported-dps="24x24"
+            fill="currentColor"
+            className="mercado-match4"
+            width="24"
+            height="24"
+            focusable="false"
           >
-            <path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z"></path>
+            <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
           </svg>
           <NavLink className="messagenav" to="/messaging">
             Messaging
@@ -111,20 +94,15 @@ export const Navbar = () => {
         <div className="navbar-container2-notification">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="19"
-            height="19"
             viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-bell-ring"
+            data-supported-dps="24x24"
+            fill="currentColor"
+            className="mercado-match5"
+            width="24"
+            height="24"
+            focusable="false"
           >
-            <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"></path>
-            <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"></path>
-            <path d="M4 2C2.8 3.7 2 5.7 2 8"></path>
-            <path d="M22 8c0-2.3-.8-4.3-2-6"></path>
+            <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
           </svg>
           <NavLink className="notificationnav" to="/notification">
             Notification
@@ -133,8 +111,8 @@ export const Navbar = () => {
         <div className="navbar-container2-me">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="19"
-            height="19"
+            width="26"
+            height="26"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -153,32 +131,30 @@ export const Navbar = () => {
         </div>
 
         {/* <section className="navbar-container3"> */}
-        <div style={{ display: "flex" }}>
+
+        <div style={{ display: "flex", alignItems:"center" }}>
+          <div className="vl">
           <div className="navbar-container3-forbusiness">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="19"
-              height="19"
               viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-grid"
+              data-supported-dps="24x24"
+              fill="currentColor"
+              className="mercado-match6"
+              width="24"
+              height="24"
+              focusable="false"
             >
-              <rect width="18" height="18" x="3" y="3" rx="2" ry="2"></rect>
-              <line x1="3" x2="21" y1="9" y2="9"></line>
-              <line x1="3" x2="21" y1="15" y2="15"></line>
-              <line x1="9" x2="9" y1="3" y2="21"></line>
-              <line x1="15" x2="15" y1="3" y2="21"></line>
+              <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
             </svg>
             <NavLink className="forbusinessnav" to="/forbusiness">
               For Business
+             
             </NavLink>
+            </div>
           </div>
 
-          <div>
+          <div style={{ paddingTop:"1rem", textAlign:"center", position:"relative"}}>
             <NavLink className="trypremiumnav" to="/trypremiumforfree">
               Try Premium for
               <span
@@ -186,7 +162,7 @@ export const Navbar = () => {
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
-                  marginRight: "1rem",
+                  marginRight: "0.8rem",
                 }}
               >
                 Free
